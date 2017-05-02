6b6ed4430559aabbfde3af3bae302a991bc40632
Shaun Cowley <eridor910@gmail.com>
2017-05-02 17:52:30 +0100

    Found the pixel coordinate of the crosshair on the screen

--- Section_04/Source/Section_04/TankPlayerController.cpp
+++ Section_04/Source/Section_04/TankPlayerController.cpp
@@ -16,7 +16,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 
 	if (GetSightRayHitLocation(HitLocation))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *HitLocation.ToString());
 
 		// TODO Tell controlled tank to aim at this point
 	}
@@ -25,7 +25,15 @@ void ATankPlayerController::AimTowardsCrosshair()
 //Get world location of linetrace through crosshair, true if it hits location
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
 {
-	OutHitLocation = FVector(1.0);
+	//Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, 
+									ViewportSizeY * CrossHairYLocation);
+
+	//De-project the screen position of the crosshair to a world direction
+	//Line trace along that look direction and see what is hit (up to max range)
 
 	return true;
 }
--- Section_04/Source/Section_04/TankPlayerController.h
+++ Section_04/Source/Section_04/TankPlayerController.h
@@ -15,6 +15,11 @@ class SECTION_04_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 private:
+	UPROPERTY(EditAnywhere)
+		float CrossHairXLocation = 0.5f;
+	UPROPERTY(EditAnywhere)
+		float CrossHairYLocation = 0.33333f;
+
 	ATank* GetControlledTank() const;
 
 	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
