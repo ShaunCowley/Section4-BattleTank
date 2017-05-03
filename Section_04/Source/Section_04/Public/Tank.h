1a4651b1c20278ce52b449df595a137968f812e0
Shaun Cowley <eridor910@gmail.com>
2017-05-03 01:03:40 +0100

    Added a tank aiming component

--- Section_04/Source/Section_04/Public/Tank.h
+++ Section_04/Source/Section_04/Public/Tank.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -24,7 +25,7 @@ private:
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
 protected:
-
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:
 	void AimAt(FVector HitLocation);
