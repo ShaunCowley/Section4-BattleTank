44ad134f60220a74b5e6a0467fb785f3cd373aa9
Shaun Cowley <eridor910@gmail.com>
2017-05-04 22:19:38 +0100

    Refactored some code to make it flow better, and removed uncessary ticking methods

--- Section_04/Source/Section_04/Public/TankBarrel.h
+++ Section_04/Source/Section_04/Public/TankBarrel.h
@@ -14,7 +14,8 @@ class SECTION_04_API UTankBarrel : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
-	void Elevate(float DegreesPerSecond);
+	// -1 is max downward movement speed, and +1 is max up movement
+	void Elevate(float RelativeSpeed);
 	
 private:
 	UPROPERTY(EditAnywhere, Category = "Setup")
