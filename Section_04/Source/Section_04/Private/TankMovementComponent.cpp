7284e9d71729f1cb66a6980790eeec91f5d6f256
Shaun Cowley <eridor910@gmail.com>
2017-05-09 18:28:39 +0100

    Finished off the movement so that left thumbstick while turn the tank, also tidied up some uncessary code

--- Section_04/Source/Section_04/Private/TankMovementComponent.cpp
+++ Section_04/Source/Section_04/Private/TankMovementComponent.cpp
@@ -6,15 +6,14 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+	if (!LeftTrack || !RightTrack) { return; }
 
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
@@ -22,4 +21,15 @@ void UTankMovementComponent::IntendMoveForward(float Throw)
 	//TODO prevent adding of values from both input types
 }
 
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Intend Turn Right At: %f"), Throw);
+
+	if (!LeftTrack || !RightTrack) { return; }
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+
+	//TODO prevent adding of values from both input types
+}
 
